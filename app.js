cca999826c5107a5c8df9998499815adeb2d5c2b
Akif <eminakif2004qgmail.com>
2026-01-22 00:03:30 +0300

    hosgeldin ve liste sayfalari ayrildi

--- app.js
+++ app.js
@@ -80,15 +80,14 @@ app.get('/logout', (req, res) => req.session.destroy(() => res.redirect('/login'
 
 // --- İŞTE EKSİK OLAN KISIMLAR GERİ GELDİ ---
 
-// 1. Kitap Listesi
-app.get('/books', requireLogin, async (req, res) => {
+// 1. Kitap Listesi Sayfası (YENİ ROTA)
+app.get('/list', requireLogin, async (req, res) => {
     const user = await User.findById(req.session.userId);
-    res.render('index', { 
-        books: user.books, 
-        totalBooks: user.books.length 
+    // Burası books.ejs'yi (Listeyi) açar
+    res.render('books', { 
+        books: user.books 
     }); 
 });
-
 // 2. Kitap Ekleme Sayfası (GET) -> ARTIK ÇALIŞACAK
 app.get('/add', requireLogin, (req, res) => {
     res.render('add-book'); 
